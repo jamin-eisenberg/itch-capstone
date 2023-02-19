8ed08c4b142439db50e1671877449d101179bf16
Spenser Dewey <spenser@dewey.fm>
2023-02-19 13:05:41 -0500

    Add some more comments, test code for RGB sensor

--- helper_functions/helper_functions.ino
+++ helper_functions/helper_functions.ino
@@ -3,13 +3,38 @@
 //    and the motor enable pins and servo pin need to be PWM pins, which the nano only has a handful of.
 //    Everything else can be moved around.
 
-// Which Side of the robot you are referencing, with Pusher being the front.
+/**************************************************************************************
+ * Library Declarations
+ **************************************************************************************/
+
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+/**************************************************************************************
+ * Data Structures
+ **************************************************************************************/
+ 
+// Which Side of the robot you are referencing, oriented as looking from hook to pusher.
 enum Side {
   RIGHT = 0b01,
   LEFT = 0b10,
   BOTH = 0b11,
 };
 
+/**************************************************************************************
+ * Global Variables
+ **************************************************************************************/
+
+/* Initialise with specific int time and gain values */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+/**************************************************************************************
+ * Constants
+ **************************************************************************************/
+ 
+// IR sensors for motor encoder, pins and constants
+const int LEFT_IR_PIN = A1;
+const int RIGHT_IR_PIN = A2;
 // Ultrasonic Sensor pins and constants.
 const int ECHO_PIN = 6;
 const int TRIG_PIN = 7;
@@ -20,7 +45,7 @@ const double WALL_THRESHOLD = 8.0; // TODO: Tweak and use in code
 const int SERVO_PIN = 9;
 const int HOOK_MIN = 125;
 const int HOOK_MAX = 255;
-const bool HOOK_UP = true
+const bool HOOK_UP = true;
 // Motor A connections
 const int RIGHT_EN = 10;
 const int RIGHT_DIR_1 = 2;
@@ -30,19 +55,23 @@ const int LEFT_EN = 11;
 const int LEFT_DIR_1 = 4;
 const int LEFT_DIR_2 = 5;
 
+/**************************************************************************************
+ * Various Helper Functions
+ **************************************************************************************/
+
 /**
  * Read the ultrasonic sensor, scaled roughly to centimeters. It may return -1 if out of range (> 4m or <2cm).
  */
  double readUltrasonicDistance() {
     float duration, distance;
-    digitalWrite(trigPin, LOW); 
+    digitalWrite(TRIG_PIN, LOW); 
     delayMicroseconds(2);
    
-    digitalWrite(trigPin, HIGH);
+    digitalWrite(TRIG_PIN, HIGH);
     delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+    digitalWrite(TRIG_PIN, LOW);
     
-    duration = pulseIn(echoPin, HIGH); // Duration of pulse in microseconds
+    duration = pulseIn(ECHO_PIN, HIGH); // Duration of pulse in microseconds
     distance = (duration / 2) * SOUND_SPEED_CONSTANT;
     
     if (distance >= 400 || distance <= 2){
@@ -110,6 +139,10 @@ void setHook(bool hookUp) {
   }
 }
 
+/**************************************************************************************
+ * Arduino Setup and Loop functions
+ **************************************************************************************/
+
 void setup() {
   // Set all the motor control pins to outputs
   pinMode(RIGHT_EN, OUTPUT);
@@ -122,11 +155,47 @@ void setup() {
   // Set Servo pin to output
   pinMode(SERVO_PIN, OUTPUT);
 
+  // Set IR pin to input
+  
+
   // Initialize pin values and global vars
-  setHook(false);
+  setHook(false); 
   stopMotor(BOTH);
 
+  Wire.begin();
+  Serial.begin(9600);
+
+  while(!Serial) {}
+
+   /* Configure TSL2550 with Extended Range */
+   if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
 }
 
 void loop() {
+//    int irLeft = analogRead(LEFT_IR_PIN);
+//    int irRight = analogRead(RIGHT_IR_PIN);
+//    Serial.print("Left: ");
+//    Serial.println((irLeft * (5.0/1023.0)));
+//    Serial.print("Right: ");
+//    Serial.println((irRight * (5.0/1023.0)));
+//    delay(100);
+  uint16_t r, g, b, c, colorTemp, lux;
+
+  tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  lux = tcs.calculateLux(r, g, b);
+
+  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+  Serial.println(" ");
 }
